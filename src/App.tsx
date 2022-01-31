6fe524eb984be1b634c7c28c8b279f0d347f502c
Leticia Caetano <leticialeitecaetano17@gmail.com>
2022-01-31 14:22:48 -0300

    🤡 add miragejs and use fetch for request

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 import { Dashboard } from './components/Dashboard';
 import { GlobalStyle } from './styles/global';
+import { createServer } from 'miragejs';
+
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 01',
+          amount: 400,
+          type: 'deposit',
+          category: 'food',
+          createdAt: new Date()
+        }
+      ]
+    });
+  }
+})
+
 
 export function App() {
   return (
