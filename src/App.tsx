ac7d156f13dee5d8aae90f12238cdcbdef4ed57c
Leticia Caetano <leticialeitecaetano17@gmail.com>
2022-02-04 16:11:43 -0300

    🎨 create new component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { createServer } from 'miragejs';
 import { useState } from 'react';
 import Modal from 'react-modal';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 createServer({
   routes(){
@@ -39,12 +40,7 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
-      <Modal
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-        >
-          Cadastrar Transação
-      </Modal>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle/>
     </>
   );
