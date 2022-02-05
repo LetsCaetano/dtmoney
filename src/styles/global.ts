c996eafc4f8bb06495e0f990988e1dc769342ae5
Leticia Caetano <leticialeitecaetano17@gmail.com>
2022-02-05 10:37:55 -0300

    🚧 styling modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -64,6 +64,7 @@ export const GlobalStyle = createGlobalStyle`
         align-items: center;
         justify-content: center;
     }
+
     .react-modal-content {
         width: 100%;
         max-width: 576px;
@@ -71,5 +72,17 @@ export const GlobalStyle = createGlobalStyle`
         padding: 3rem;
         position: relative;
         border-radius: 0.24rem;
+    }
+
+    .react-modal-close {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+
+        &:hover {
+            filter: brightness(0.9)
+        }
     }
 `
