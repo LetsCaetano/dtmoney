f458f287f89944d6a75a30fc7ec44d4ab12e4007
Leticia Caetano <leticialeitecaetano17@gmail.com>
2022-02-06 17:41:11 -0300

    🚧 buttons operation

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -54,22 +54,28 @@ export const TransactionTypeContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
+`;
 
-    button {
-        height: 4rem;
-        border: 1px solid #d7d7d7;
-        background: transparent;
 
-        display: flex;
-        align-items: center;
-        justify-content: center;
+interface RadioBoxProps {
+    isActive: boolean;
+}
 
-        transition: border-color 0.2s;
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    background: ${(props) => props.isActive ? '#ccc':'trasparent' };
 
-        &:hover {
-            border-color: ${darken(0.1, '#d7d7d7')};
-        }
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: border-color 0.2s;
+
+    &:hover {
+        border-color: ${darken(0.1, '#d7d7d7')};
     }
+
 
     img {
         width: 20px;
