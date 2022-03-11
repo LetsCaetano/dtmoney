a3c9231176adc0c2b734ff3cf7a8d1a8374a0cc0
Leticia Caetano <leticialeitecaetano17@gmail.com>
2022-03-11 16:39:33 -0300

    🚧 finish inseeting

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -29,7 +29,7 @@ interface TransactionsContextProps {
 
 interface TransactionsContextData {
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -45,8 +45,23 @@ export function TransactionsProvider({ children }:TransactionsContextProps) {
         .then(response => setTransactions(response.data.transactions))
     }, []);
 
-    function createTransaction(transaction: TransactionInput){
-        api.post('/transactions',transaction);
+    async function createTransaction(transactionInput: TransactionInput){
+        const response = await api.post(
+            '/transactions',
+            {
+                ...transactionInput,
+                createdAt: new Date(),
+            }
+        );
+
+
+        const { transaction } = response.data;
+
+        setTransactions([
+            ...transactions,
+            transaction
+        ])
+
     }
 
     return (
