9bcd58c2265016392eb2fc14ba4d9bb9422baf99
Leticia Caetano <leticialeitecaetano17@gmail.com>
2023-03-12 10:05:02 -0300

    refactor: improve code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Dashboard } from './components/Dashboard';
-import { GlobalStyle } from './styles/global';
 import { createServer, Model } from 'miragejs';
 import { useState } from 'react';
 import Modal from 'react-modal';
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { TransactionsProvider } from './hooks/useTransactions';
+import { GlobalStyle } from './styles/global';
 
 
 createServer({
